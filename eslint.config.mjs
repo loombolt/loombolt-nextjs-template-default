10296746a4c3a4aede8069f8d11d8138d2a03c0d
Rinzy <atermizor@gmail.com>
2025-08-04 13:43:27 -0500

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,7 +16,7 @@ const eslintConfig = [
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
-        "react/no-unescaped-entities": "of f",
+       "reat/no-unescaped-entities": "off",
       "@typescript-eslint/no-unsafe-assignment": "off",
       "@typescript-eslint/no-unsafe-member-access": "off",
       "@typescript-eslint/ban-ts-comment": "off",
